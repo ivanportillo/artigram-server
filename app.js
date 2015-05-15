bf2fe750e595ce4abb70583fe4ca9717f7efe8f5
Iván Portillo Leal <ivanportilloleal@IvanPortillo.local>
2015-05-15 22:57:37 +0200

    Conseguida conexion con telnet (CODIGO AUN NO REFACTORIZADO)

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var telnet = require('net'),
-    // http = require('http'),
-    // express = require('express'),
-    // app = express(),
-    exec = require('child_process').exec,
+var exec = require('child_process').exec,
     home = process.env.HOME,
     user = process.env.USER,
-    command = "cd " + home + "/tg/ && bin/telegram-cli -vvvvRC -k tg-server.pub -W -dL tg.log -P 1234",
-    child = exec(command);
+    command = "cd " + home + "/tg/ && bin/telegram-cli -vvvvRC -k tg-server.pub -W -dL tg.log -P 1234 -l 1 -E",
+    child = exec(command),
+    connected = false,
+    http = require('http'),
+    express = require('express'),
+    app = express(),
+    bool;
 
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
@@ -18,21 +19,34 @@ process.on('exit', function(){
   console.log("EXIT NODE");
 });
 
-process.on('SIGINT', function(){
-  console.log("CTRL+C Pressed");
-
-});
-
 process.on('uncaughtException', function(){
   console.log("uncaughtException");
 });
 
-var ip = '127.0.0.1';
-var port = 1234;
+var net = require('net');
+var conn = net.createConnection(1234,'127.0.0.1');
 
-var client = net.connect(port, ip, function(){
-	//client.write("msg ASL holiii");
-	client.on('data', function(data){
-		console.log("CONECTADITO");
-	});
+
+function sendData(socket, data) {
+    socket.write(data + "\n");
+}
+
+conn.on('connect', function(){
+  console.log("Connected to telnet server.");
 });
+
+conn.on('data', function(data){
+  console.log(data.toString());
+});
+
+conn.on('close', function(){
+  conn.end();
+});
+
+app.get('/', function(req, res){
+    sendData(conn, "msg ASL testNODE2");
+    conn.end();
+    res.send("Message sent!");
+});
+
+app.listen(8080);
